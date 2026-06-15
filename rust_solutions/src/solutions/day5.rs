6bc873d8e2717626d0a3076c59731a3de8920456
Nico Govindsamy <ngovindsamy18@gmail.com>
2026-06-15 20:01:13 +0200

    day 5 part 2

--- rust_solutions/src/solutions/day5.rs
+++ rust_solutions/src/solutions/day5.rs
@@ -34,3 +34,45 @@ pub fn sol(input: &str) -> usize {
         return count;
     })
 }
+
+pub fn sol2(input: &str) -> usize {
+    let id_ranges = input.split_once("\n\n").unwrap().0;
+
+    let mut id_ranges: Vec<(usize, usize)> = id_ranges
+        .lines()
+        .filter_map(|line| {
+            let line = line.trim();
+            if line.is_empty() {
+                return None;
+            }
+
+            let (a, b) = line.split_once('-')?;
+            Some((a.parse::<usize>().unwrap(), b.parse::<usize>().unwrap()))
+        })
+        .collect();
+
+    id_ranges.sort();
+
+    let mut num_ids = 0;
+    for i in 0..id_ranges.len() {
+        let (a, mut b) = id_ranges[i];
+
+        if let Some((c, d)) = id_ranges.get(i + 1) {
+            if b >= *c {
+                let right = c - 1;
+
+                if b > *d {
+                    id_ranges[i + 1].1 = b;
+                }
+
+                b = right;
+            }
+        }
+
+        if b >= a {
+            num_ids += b - a + 1;
+        }
+    }
+
+    num_ids
+}
