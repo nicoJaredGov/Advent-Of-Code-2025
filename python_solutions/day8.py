7c23c477ab9757ba0ef8541ce11fd30e261a3dc6
Nico Govindsamy <ngovindsamy18@gmail.com>
2026-07-03 14:12:27 +0200

    day 8 cleaned up code

--- python_solutions/day8.py
+++ python_solutions/day8.py
@@ -1,25 +1,54 @@
 import numpy as np
 import os
 import time
+import functools
 
+def timer_wrapper(func):
+    """A decorator that prints the execution time of a function."""
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        
+        execution_time = end_time - start_time
+        print(f"Executed in {execution_time:.6f} seconds\nResult: {result}\n")
+        
+        return result
+    
+    return wrapper
+
+def get_distance_mat(X: np.ndarray[any]):
+    '''
+        Given a 2-d matrix of points, calculates the distance matrix for each pair of points.
+
+        Parameters
+        ----------
+        X : array-like
+            (n x d) matrix representing n points in d dimensions
+    '''
+
+    # Compute Gram matrix
+    G = np.dot(X, X.T)
+
+    # Get squared norms (diagonal of G)
+    # Reshape to (n, 1) to enable matrix broadcasting
+    p = np.diag(G).reshape(-1, 1)
+
+    # Combine using broadcasting and take square root
+    # p + p.T automatically creates the (n x n) matrix addition grid
+    D = p + p.T - 2 * G
+
+    return D
+    
+@timer_wrapper
 def sol(file_name: str, num_connections: int = 10) -> int:
-    start_time = time.perf_counter()
-
     file_path = os.path.abspath(f'{os.path.dirname(__file__)}/{file_name}.txt')
     with open(file_path, mode="r") as file:
         # X is an (n x d) matrix representing n points in d dimensions
         X = np.array([[int(x) for x in l.split(',')] for l in file.read().splitlines()])
 
-        # Step 2: Compute Gram matrix
-        G = np.dot(X, X.T)
-
-        # Step 3: Get squared norms (diagonal of G)
-        # Reshape to (n, 1) to enable matrix broadcasting
-        p = np.diag(G).reshape(-1, 1)
-
-        # Step 4 & 5: Combine using broadcasting and take square root
-        # p + p.T automatically creates the (n x n) matrix addition grid
-        D = p + p.T - 2 * G
+    D = get_distance_mat(X)
 
     # 1. Get the coordinate arrays (creates index arrays, no matrix data is copied)
     r, c = np.triu_indices_from(D, k=1)
@@ -38,55 +67,32 @@ def sol(file_name: str, num_connections: int = 10) -> int:
     positions: dict[int, int] = dict()
     id = 0
     for (conn1, conn2) in sorted_pairs:
-        found = False
+        first = positions.get(conn1)
+        second = positions.get(conn2)
 
-        for (i, circuit) in circuits.items():
-            first_found = conn1 in circuit
-            second_found = conn2 in circuit
-
-            if first_found and second_found:
-                found = True
-                break
-    
-            if first_found or second_found:
-                other_junction = conn2 if first_found else conn1
-                circuit.add(other_junction)
-
-                # Merge the other junction's circuit with current circuit
-                if other_junction in positions:
-                    original_pos = positions[other_junction]
-
-                    for junction in circuits[original_pos]:
-                        circuit.add(junction)
-                        positions[junction] = i
-                    
-                    del(circuits[original_pos])
-                else:
-                    positions[other_junction] = i
-
-                found = True
-                break
-
-        if not found:
+        if first is None and second is None:
             circuits[id] = {conn1, conn2}
             positions[conn1] = id
             positions[conn2] = id
-
             id += 1
+        elif first is None:
+            positions[conn1] = second
+            circuits[second].add(conn1)
+        elif second is None:
+            positions[conn2] = first
+            circuits[first].add(conn2)
+        elif first != second:
+            for junction in circuits[second]:
+                circuits[first].add(junction)
+                positions[junction] = first
+            del(circuits[second])
         
         num_connections -= 1
-
         if num_connections == 0:
             break
 
     sizes = sorted(map(lambda c: len(c), circuits.values()))
     result = np.prod(sizes[-1:-4:-1])
-
-    end_time = time.perf_counter()
-    elapsed_time = end_time - start_time
-
-    print(f"Execution time: {elapsed_time:.4f} seconds")
-    print(result)
 
     return result
 
