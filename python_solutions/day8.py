db8b0dcd1395b560cfec9a8c05b77bddf6ec0c48
Nico Govindsamy <ngovindsamy18@gmail.com>
2026-07-03 15:12:30 +0200

    day 8 part 2

--- python_solutions/day8.py
+++ python_solutions/day8.py
@@ -24,16 +24,8 @@ def get_distance_mat(X: np.ndarray[any]):
     D = p + p.T - 2 * G
 
     return D
-    
-@timer_wrapper
-def sol(file_name: str, num_connections: int = 10) -> int:
-    file_path = os.path.abspath(f'{os.path.dirname(__file__)}/{file_name}.txt')
-    with open(file_path, mode="r") as file:
-        # X is an (n x d) matrix representing n points in d dimensions
-        X = np.array([[int(x) for x in l.split(',')] for l in file.read().splitlines()])
 
-    D = get_distance_mat(X)
-
+def get_sorted_pairs(D: np.ndarray[any]):
     # 1. Get the coordinate arrays (creates index arrays, no matrix data is copied)
     r, c = np.triu_indices_from(D, k=1)
 
@@ -47,9 +39,11 @@ def sol(file_name: str, num_connections: int = 10) -> int:
     # View the result as pairs
     sorted_pairs = list(zip(sorted_r, sorted_c))
 
-    circuits: dict[int, set] = dict()
-    positions: dict[int, int] = dict()
+    return sorted_pairs
+
+def build_circuits(circuits, positions, sorted_pairs, evaluate_terminating):
     id = 0
+
     for (conn1, conn2) in sorted_pairs:
         first = positions.get(conn1)
         second = positions.get(conn2)
@@ -71,14 +65,61 @@ def sol(file_name: str, num_connections: int = 10) -> int:
                 positions[junction] = first
             del(circuits[second])
         
-        num_connections -= 1
-        if num_connections == 0:
-            break
+        if evaluate_terminating():
+            return (conn1, conn2)
+    
+@timer_wrapper
+def sol(file_name: str, num_connections: int = 10) -> int:
+    file_path = os.path.abspath(f'{os.path.dirname(__file__)}/{file_name}.txt')
+    with open(file_path, mode="r") as file:
+        X = np.array([[int(x) for x in l.split(',')] for l in file.read().splitlines()])
 
+    D = get_distance_mat(X)
+    sorted_pairs = get_sorted_pairs(D)
+
+    circuits: dict[int, set] = dict()
+    positions: dict[int, int] = dict()
+
+    def evaluate_terminating():
+        nonlocal num_connections
+        num_connections -= 1
+        return num_connections == 0
+    
+    build_circuits(circuits, positions, sorted_pairs, evaluate_terminating)
+    
     sizes = sorted(map(lambda c: len(c), circuits.values()))
     result = np.prod(sizes[-1:-4:-1])
 
     return result
 
+@timer_wrapper
+def sol2(file_name: str) -> int:
+    file_path = os.path.abspath(f'{os.path.dirname(__file__)}/{file_name}.txt')
+    with open(file_path, mode="r") as file:
+        X = np.array([[int(x) for x in l.split(',')] for l in file.read().splitlines()])
+
+    D = get_distance_mat(X)
+    sorted_pairs = get_sorted_pairs(D)
+
+    circuits: dict[int, set] = dict()
+    positions: dict[int, int] = dict()
+
+    def evaluate_terminating():
+        if len(circuits) != 1:
+            return False
+        
+        for circuit in circuits.values():
+            if len(circuit) == len(X):
+                return True
+            
+        return False
+
+    (conn1, conn2) = build_circuits(circuits, positions, sorted_pairs, evaluate_terminating)
+    result = X[conn1][0] * X[conn2][0]
+
+    return result
+    
 sol("../inputs/example/day8", num_connections=10)
 sol("../inputs/day8", num_connections=1000)
+sol2("../inputs/example/day8")
+sol2("../inputs/day8")
