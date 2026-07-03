2a4ab34193e3dd83d175fb1c18a70fdb990d197a
Nico Govindsamy <ngovindsamy18@gmail.com>
2026-07-03 14:26:57 +0200

    add python utils

--- python_solutions/day8.py
+++ python_solutions/day8.py
@@ -1,22 +1,6 @@
 import numpy as np
 import os
-import time
-import functools
-
-def timer_wrapper(func):
-    """A decorator that prints the execution time of a function."""
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        start_time = time.perf_counter()
-        result = func(*args, **kwargs)
-        end_time = time.perf_counter()
-        
-        execution_time = end_time - start_time
-        print(f"Executed in {execution_time:.6f} seconds\nResult: {result}\n")
-        
-        return result
-    
-    return wrapper
+from utils import timer_wrapper
 
 def get_distance_mat(X: np.ndarray[any]):
     '''
